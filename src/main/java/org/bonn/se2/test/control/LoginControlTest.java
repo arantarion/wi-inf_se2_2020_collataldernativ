5ae4653f72e5e9543a9f21470e65f4790fa763be
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-05-13 19:26:10 +0200

    Added LoginControlTest Class

---
+++ src/main/java/org/bonn/se2/test/control/LoginControlTest.java
@@ -0,0 +1,21 @@
+package org.bonn.se2.test.control;
+
+import org.bonn.se2.model.objects.dto.UserAtLogin;
+import org.bonn.se2.process.control.LoginControl;
+import org.bonn.se2.process.control.exceptions.InvalidCredentialsException;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LoginControlTest {
+
+    @Test
+    public void checkAuthenticationWithEmptyCredentials() {
+        UserAtLogin user = new UserAtLogin("", "");
+        assertThrows(InvalidCredentialsException.class, () -> LoginControl.checkAuthentication(user));
+    }
+
+    @Test
+    public void getRole() {
+    }
+}
