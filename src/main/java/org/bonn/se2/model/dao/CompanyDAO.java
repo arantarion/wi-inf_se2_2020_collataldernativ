4e9d2019c93786da59c938b372310f26f8f01ea8
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-05-15 16:54:42 +0200

    started work on class

--- src/main/java/org/bonn/se2/model/dao/CompanyDAO.java
+++ src/main/java/org/bonn/se2/model/dao/CompanyDAO.java
@@ -1,4 +1,84 @@
 package org.bonn.se2.model.dao;
 
-public class CompanyDAO {
+import org.bonn.se2.model.objects.dto.Company;
+import org.bonn.se2.process.control.exceptions.DatabaseException;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+public class CompanyDAO extends AbstractDAO<Company> implements DAOInterface<Company> {
+
+    public CompanyDAO() throws DatabaseException {
+    }
+
+    @Override
+    public Company retrieve(int id) throws Exception {
+        String sql =
+                "SELECT * FROM \"CollDB\".\"user\"\n" +
+                        "         JOIN \"CollDB\".company ON \"user\".userID = company.userID\n" +
+                        "         JOIN \"CollDB\".address on \"user\".userID = address.userID\n" +
+                        "WHERE companyID = " + id + ";";
+
+        List<Company> result = execute(sql);
+        if (result.size() < 1) {
+            throw new DatabaseException("retrieve(int id) failed");
+        }
+        return result.get(0);
+    }
+
+    @Override
+    public Company retrieve(String attribute) throws Exception {
+        String query =
+                "SELECT * FROM \"CollDB\".company\n" +
+                        "JOIN \"CollDB\".user ON company.userID = \"user\".userID\n" +
+                        "JOIN \"CollDB\".address ON \"user\".userID = address.userID\n" +
+                        "WHERE username = '" + attribute + "'\n" +
+                        "OR email = '" + attribute + "'\n" +
+                        "OR companyname = '" + attribute + "';";
+
+        List<Company> list = execute(query);
+        if (list.size() < 1) {
+            throw new DatabaseException("retrieve(String attribute) failed");
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public List<Company> retrieveAll() throws Exception {
+        String query =
+                "SELECT * FROM \"CollDB\".company\n" +
+                        "JOIN \"CollDB\".\"user\" ON company.userID = \"user\".userID\n" +
+                        "JOIN \"CollDB\".address ON \"user\".userID = address.userID;";
+        return execute(query);
+    }
+
+    @Override
+    public Company create(Company dto) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected Company create(ResultSet resultSet) throws DatabaseException {
+        return null;
+    }
+
+    @Override
+    public Company update(Company item) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Company delete(Company company) throws Exception {
+        final String deleteQuery =
+                "DELETE FROM \"CollDB\".user\n" +
+                        "WHERE username = ?\n" +
+                        "RETURNING *;";
+
+        List<Company> result = executePrepared(deleteQuery, company.getUsername());
+        if (result.size() < 1) {
+            throw new DatabaseException("delete(Company company) failed");
+        }
+        return result.get(0);
+    }
+
 }
