c6dbbc76d8650d8612f7180afb0799bd9f19f374
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-05-16 17:45:17 +0200

    fixed SQL (?) and Exceptions

--- src/main/java/org/bonn/se2/model/dao/AddressDAO.java
+++ src/main/java/org/bonn/se2/model/dao/AddressDAO.java
@@ -5,6 +5,7 @@ import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.process.control.exceptions.DontUseException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AddressDAO extends AbstractDAO<Address> implements DAOInterface<Address>{
@@ -14,14 +15,15 @@ public class AddressDAO extends AbstractDAO<Address> implements DAOInterface<Add
 
     @Override
     public Address retrieve(int id) throws DatabaseException {
+        //language=PostgreSQL
         final String sql =
-                "SELECT strasse, hausnummer, plz, stadt, land, addressID\n" +
-                "FROM \"CollDB\".address\n" +
-                "WHERE addressID = ?;";
+                "SELECT strasse, hausnummer, plz, stadt, land, \"addressID\" " +
+                "FROM \"collDB\".address " +
+                "WHERE \"addressID\" = '" + id +"';";
 
-        List<Address> result = executePrepared(sql, id);
+        List<Address> result = execute(sql);
         if (result.size() < 1) {
-            throw new DatabaseException("Something's wrong I can feel it");
+            throw new DatabaseException("retrieve(int id) in AddressDAO failed");
         }
         return result.get(0);
     }
@@ -33,20 +35,48 @@ public class AddressDAO extends AbstractDAO<Address> implements DAOInterface<Add
 
     @Override
     public List<Address> retrieveAll() throws DatabaseException  {
+        //language=PostgreSQL
         final String sql =
-                "SELECT strasse, hausnummer, plz, stadt, land, addressID\n" +
-                "FROM \"CollDB\".address;";
+                "SELECT strasse, hausnummer, plz, stadt, land, \"addressID\" " +
+                "FROM \"collDB\".address;";
         return execute(sql);
     }
 
     @Override
-    public Address create(Address dto) throws Exception {
-        return null;
+    public Address create(Address dto) throws DatabaseException {
+        //language=PostgreSQL
+        String query = "INSERT INTO \"collDB\".address " +
+                "VALUES (?, ?, ?, ?, ?, DEFAULT) RETURNING strasse, hausnummer, plz, stadt, land, \"addressID\";";
+
+        List<Address> list = executePrepared(query,
+                dto.getStrasse(),
+                dto.getHausnummer(),
+                dto.getPlz(),
+                dto.getStadt(),
+                dto.getLand()
+        );
+
+        if (list.size() == 0)
+            throw new DatabaseException("create(Address dto) in AddressDAO failed");
+        return list.get(0);
     }
 
     @Override
     protected Address create(ResultSet resultSet) throws DatabaseException {
-        return null;
+        Address dto;
+        try {
+            dto = new Address();
+            dto.setStrasse(resultSet.getString("strasse"));
+            dto.setHausnummer((resultSet.getString("hausnummer")));
+            dto.setPlz(resultSet.getString("plz"));
+            dto.setStadt(resultSet.getString("stadt"));
+            dto.setLand(resultSet.getString("land"));
+            dto.setID(resultSet.getInt("addressID"));
+
+        } catch (SQLException e) {
+            throw new DatabaseException("[" + AddressDAO.class.toString() + "] Error creating Address: " + e.getMessage());
+        }
+        return dto;
     }
 
 
@@ -57,14 +87,15 @@ public class AddressDAO extends AbstractDAO<Address> implements DAOInterface<Add
 
     @Override
     public Address delete(Address address) throws Exception {
+        //language=PostgreSQL
         final String sql =
-                "DELETE FROM \"CollDB\".address\n" +
-                "WHERE addressID = ? " +
+                "DELETE FROM \"collDB\".address " +
+                "WHERE \"addressID\" = ? " +
                 "RETURNING *;";
         
         List<Address> list = executePrepared(sql, address.getID());
         if (list.size() == 0)
-            throw new DatabaseException("There are no mistakes. Just happy little accidents!");
+            throw new DatabaseException("delete(Address address) in AddressDAO failed");
         return list.get(0);
     }
 }
--- src/main/java/org/bonn/se2/model/dao/RoleDAO.java
+++ src/main/java/org/bonn/se2/model/dao/RoleDAO.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Deprecated
 public class RoleDAO { //extends AbstractDAO
 
     public static RoleDAO dao = null;
