56696cf3d176674d8895d508c6c285ec5bb57209
Anton Drees <anton.drees@smail.inf.h-brs.de>
2020-06-17 11:43:18 +0200

    Added to DocumentDAO: retrieve(fileID), retrieveAll, delete(file) and delete(fileID)

--- src/main/java/org/bonn/se2/model/dao/DocumentDAO.java
+++ src/main/java/org/bonn/se2/model/dao/DocumentDAO.java
@@ -3,9 +3,12 @@ package org.bonn.se2.model.dao;
 
 import org.bonn.se2.model.objects.dto.Document;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
+import org.bonn.se2.process.control.exceptions.InvalidCredentialsException;
 
 import java.sql.ResultSet;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Coll@Aldernativ
@@ -19,13 +22,18 @@ public class DocumentDAO extends AbstractDAO<Document> implements DAOInterface<D
     }
 
     @Override
-    protected Document create(ResultSet resultSet) throws DatabaseException {
-        return null;
-    }
+    public Document retrieve(int fileID) throws Exception {
+        //language=PostgreSQL
+        final String sql =
+                "SELECT * FROM \"collDB\".file " +
+                        "WHERE file.\"fileID\" = '" + fileID + "';";
 
-    @Override
-    public Document retrieve(int id) throws Exception {
-        return null;
+        List<Document> result = execute(sql);
+        if (result.size() < 1) {
+            throw new InvalidCredentialsException();
+        }
+        Logger.getLogger(DocumentDAO.class.getName()).log(Level.INFO, "Das Document-Objekt mit der fileID: " + fileID + " wurde abgerufen.");
+        return result.get(0);
     }
 
     @Override
@@ -35,11 +43,18 @@ public class DocumentDAO extends AbstractDAO<Document> implements DAOInterface<D
 
     @Override
     public List<Document> retrieveAll() throws Exception {
-        return null;
+        final String sql = "SELECT * FROM \"collDB\".file";
+        Logger.getLogger(DocumentDAO.class.getName()).log(Level.INFO, "Alle Documents wurden abgerufen.");
+        return execute(sql);
     }
 
     @Override
     public Document create(Document dto) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected Document create(ResultSet resultSet) throws DatabaseException {
         return null;
     }
 
@@ -49,7 +64,23 @@ public class DocumentDAO extends AbstractDAO<Document> implements DAOInterface<D
     }
 
     @Override
-    public Document delete(Document item) throws Exception {
-        return null;
+    public Document delete(Document file) throws Exception {
+        //language=PostgreSQL
+        final String deleteQuery = " DELETE FROM \"collDB\".file WHERE \"fileID\" = " + file.getDocumentID() + " RETURNING * ";
+        List<Document> result = execute(deleteQuery);
+        if (result.size() < 1) {
+            throw new DatabaseException("delete(Document file) failed");
+        }
+        return result.get(0);
+    }
+
+    public Document delete(int fileID) throws Exception{
+        //language=PostgreSQL
+        final String deleteQuery = "DELETE FROM \"collDB\".file WHERE \"fileID\" = " + fileID + " RETURNING *";
+        List<Document> result = execute(deleteQuery);
+        if (result.size() < 1){
+            throw new DatabaseException("delete(int fileID) failed");
+        }
+        return result.get(0);
     }
 }
