b9bae764cd95029f3dae2fd7f13c222515258713
Zuckerbonbon <Zuckerbonbon@Zuckerbonbon-PC.fritz.box>
2020-06-25 10:17:29 +0200

    Preparing Togglebar (LoginView Line 49-99)

--- src/main/java/org/bonn/se2/gui/views/LoginView.java
+++ src/main/java/org/bonn/se2/gui/views/LoginView.java
@@ -1,21 +1,29 @@
 package org.bonn.se2.gui.views;
 
-import com.vaadin.event.ShortcutAction;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.server.Page;
-import com.vaadin.shared.Position;
-import com.vaadin.ui.*;
 import org.bonn.se2.gui.ui.MyUI;
-import org.bonn.se2.gui.windows.SendCanditureWindow;
-import org.bonn.se2.model.objects.dto.Student;
+import org.bonn.se2.model.dao.ToggleDAO;
 import org.bonn.se2.model.objects.dto.User;
 import org.bonn.se2.model.objects.dto.UserAtLogin;
 import org.bonn.se2.process.control.LoginControl;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.process.control.exceptions.InvalidCredentialsException;
 import org.bonn.se2.services.util.Configuration;
+
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.RadioButtonGroup;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  * @author Coll@Aldernativ
@@ -37,7 +45,58 @@ public class LoginView extends VerticalLayout implements View {
     }
 
     private void setUp() {
-    	
+    	//TODO TOGGLE
+    	/*
+    	RadioButtonGroup<String> toggle = new RadioButtonGroup<>("Bewerbungen zulassen");
+        toggle.setItems("Ja", "Nein");
+        ToggleDAO dao = null;
+		try {
+			dao = new ToggleDAO();
+		} catch (DatabaseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        try {
+			if( dao.retrieve() == true) {
+				toggle.setSelectedItem("Ja"); 
+			} else if (dao.retrieve() == false) {
+				toggle.setSelectedItem("Nein"); 
+			} else {
+				System.out.println("Nix geladen");
+			}
+		} catch (DatabaseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        toggle.addSelectionListener(item -> {
+        	if(item == null) {
+        		System.out.println("Nix gewählt");
+        	} else if (item.getValue() == "Ja"){
+        		try {
+        			ToggleDAO dao2 = new ToggleDAO();
+					dao2.updateToggle(true);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        	} else if (item.getValue() == "Nein") {
+				try {
+					ToggleDAO dao2 = new ToggleDAO();
+					dao2.updateToggle(true);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        		
+        	} else {
+        		
+        	}
+        		
+        });
+        	
+        
+        this.addComponent(toggle);
+    	*/
 
         this.setSizeFull();
 
