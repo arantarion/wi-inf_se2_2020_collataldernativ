ab463640ab3995203b7d1b64fea6f60d5b64b6a4
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-06-30 18:51:53 +0200

    updated

--- src/test/java/org/bonn/se2/test/model/dao/AddressDAOTest.java
+++ src/test/java/org/bonn/se2/test/model/dao/AddressDAOTest.java
@@ -1,23 +1,17 @@
 package org.bonn.se2.test.model.dao;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bonn.se2.model.dao.AddressDAO;
 import org.bonn.se2.model.objects.dto.Address;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.test.builder.AddressBuilder;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Coll@Aldernativ
@@ -27,44 +21,18 @@ import org.junit.jupiter.api.Test;
 
 public class AddressDAOTest {
 
-    private static Address newAddress;
-    private Address testAddress, updatedAddress;
     private static AddressDAO dao;
-    private static int testAddressId;
     private static List<Address> listAddress;
-
-    public static int getTestAddressId() {
-        return testAddressId;
-    }
-
-    public static void setTestAddressId(int testAddressId) {
-        AddressDAOTest.testAddressId = testAddressId;
-    }
 
     @BeforeAll
     static void beforeAll() {
-    	listAddress = new ArrayList<>();
+        listAddress = new ArrayList<>();
         try {
             dao = new AddressDAO();
         } catch (DatabaseException e) {
             e.printStackTrace();
             fail();
         }
-        newAddress = AddressBuilder.getInstance()
-                .createDefaultAddress()
-                .withStreet("Straße")
-                .withHousenumber("1")
-                .withPostalcode("12345")
-                .withCity("Stadt")
-                .withCountry("Land")
-                .get();
-        System.out.println(newAddress);
-    }
-
-    @BeforeEach
-    void setUp() {
-        testAddress = null;
-        updatedAddress = null;
     }
 
     @Test
@@ -77,78 +45,22 @@ public class AddressDAOTest {
             e.printStackTrace();
             fail();
         }
-        assertTrue(listAddress.size() >= 0);
+        assertTrue(listAddress.size() > 0);
     }
-/*
+
     @Test
     @Order(2)
-    public void create() {
-        try {
-            assertNull(testAddress);
-            dao.create(newAddress);
-            assertNotEquals(0, testAddress.getID());
-            setTestAddressId(testAddress.getID());
-            testDTO(testAddress);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    @Test
-    @Order(3)
     public void retrieve() {
+        Address address = new Address();
         try {
-            assertNull(testAddress);
-            testAddress = dao.retrieve(getTestAddressId());
-            testDTO(testAddress);
+            address = dao.retrieve(7);
         } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
+        assertEquals(address.getStrasse(), "Waldstr");
+        assertEquals(address.getHausnummer(), "99");
+        assertEquals(address.getLand(), "Deutschland");
     }
 
-    @Test
-    @Order(4)
-    public void updateOne() {
-        assertNull(testAddress);
-        try {
-            testAddress = dao.retrieve(getTestAddressId());
-            testAddress.setStrasse("Andere Straße");
-            testAddress.setHausnummer("2B");
-            testAddress.setPlz("54321");
-            updatedAddress = dao.update(testAddress);
-            assertEquals("Andere Straße", updatedAddress.getStrasse());
-            assertEquals("2B", updatedAddress.getHausnummer());
-            assertEquals("54321", updatedAddress.getPlz());
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-            fail();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    @Order(5)
-    public void deleteOne() throws Exception {
-        assertNull(testAddress);
-        try {
-            testAddress = dao.retrieve(getTestAddressId());
-            dao.delete(testAddress);
-            assertThrows(DatabaseException.class, () -> dao.retrieve(getTestAddressId()));
-        } catch (DatabaseException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-*/
-    private void testDTO(Address dto) {
-        assertNotEquals(0, dto.getID());
-        assertEquals("Straße", dto.getStrasse());
-        assertEquals("1", dto.getHausnummer());
-        assertEquals("12345", dto.getPlz());
-        assertEquals("Stadt", dto.getStadt());
-        assertEquals("Land", dto.getLand());
-    }
 }
--- src/test/java/org/bonn/se2/test/model/dao/BewerbungDAOTest.java
+++ src/test/java/org/bonn/se2/test/model/dao/BewerbungDAOTest.java
@@ -1,12 +1,5 @@
 package org.bonn.se2.test.model.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.List;
-import java.sql.ResultSet;
-
 import org.bonn.se2.model.dao.BewerbungsDAO;
 import org.bonn.se2.model.objects.dto.Bewerbung;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
@@ -14,231 +7,185 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/*
-@author Coll@Aldernativ
-@version 0.1a
-@Programmer Marceli Dziadek
-*/
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Coll@Aldernativ
+ * @version 0.1a
+ * @Programmer Marceli Dziadek
+ */
 
 class BewerbungDAOTest {
-	
-	static Bewerbung bewerbung;
-	static ResultSet resultset;
-	static BewerbungsDAO dao;
-	static List<Bewerbung> listeBewerbung;
-	static List<Bewerbung> listeBewerbung2;
-	static int bewerbungID;
-	static int companyID;
-	static int jobOfferID;
-	static int size;
-	static int studentID;
-	
-	@BeforeAll
-	static void beforeAll() {
-		try {
-			dao = new BewerbungsDAO();
-			bewerbungID = dao.retrieveAll().get(0).getBewerbungsID();
-			companyID = dao.retrieveAll().get(0).getCompanyID();
-			jobOfferID = dao.retrieveAll().get(0).getJobofferID();
-			studentID = dao.retrieveAll().get(0).getStudentID();
-		} catch (DatabaseException e) {
-			fail();
-		} catch (Exception e) {
-			fail();
-		}
-		
-		
-	}
-	
-	@BeforeEach
-	void beforeEach() {
-		try {
-			size = dao.retrieveAll().size();
-		} catch (Exception e) {
-			fail();
-		}
-	}
 
-	@Test
-	void Testretrieve() {
-		try {
-			bewerbung = dao.retrieve(bewerbungID);
-		} catch (Exception e) {
-			fail();
-		}
-		assertEquals(bewerbung.getBewerbungsID(),bewerbungID);
-		assertEquals(bewerbung.getJobofferID(), jobOfferID);
-		assertEquals(bewerbung.getCompanyID(), companyID);
-		
-		assertNotNull(bewerbung.getBewerbungsdatum());
-		assertNotNull(bewerbung.getNotes());
-	}
-	
-	@Test
-	void TestretrieveCompanyBewerbungJobOffer() {
-		try {
-			listeBewerbung = dao.retrieveCompanyBewerbungJobOffer(companyID, jobOfferID);
-			size = listeBewerbung.size();
-		} catch (DatabaseException e) {
-			fail();
-		} catch (SQLException e) {
-			fail();
-		}
-		assertNotNull(listeBewerbung);
-		assertFalse(listeBewerbung.isEmpty());
-		
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getJobofferID(),jobOfferID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getJobofferID(),jobOfferID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getJobofferID(),jobOfferID);
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-	}
-	
-	@Test
-	void TestretrieveCompanyBewerbung() {
-		try {
-			listeBewerbung = dao.retrieveCompanyBewerbung(companyID);
-			size = listeBewerbung.size();
-		} catch (DatabaseException e) {
-			fail();
-		} catch (SQLException e) {
-			fail();
-		}
-		assertNotNull(listeBewerbung);
-		assertFalse(listeBewerbung.isEmpty());
-		
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		assertEquals(listeBewerbung.get((int)Math.random()*size).getCompanyID(),companyID);
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-	}
-	
-	@Test
-	void TestretrieveAll() {
-		try {
-			listeBewerbung = dao.retrieveAll();
-			size = listeBewerbung.size();
-		} catch (Exception e) {
-			fail();
-		}
-		assertNotNull(listeBewerbung);
-		assertFalse(listeBewerbung.isEmpty());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getCompanyID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getCompanyID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getCompanyID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getJobofferID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getBewerbungsdatum());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getStudentID());
-		
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-		assertNotNull(listeBewerbung.get((int)Math.random()*size).getNotes());
-	}
-	
-	@Test
-	void Testcreate() {
-		bewerbung = new Bewerbung();
-		bewerbung.setCompanyID(companyID);
-		bewerbung.setJobofferID(jobOfferID);
-		bewerbung.setBewerbungsID(0);
-		bewerbung.setNotes("");
-		bewerbung.setStudentID(studentID);
-		bewerbung.setBewerbungsdatum(LocalDate.now());
+    static Bewerbung bewerbung;
+    static BewerbungsDAO dao;
+    static List<Bewerbung> listeBewerbung;
+    static List<Bewerbung> listeBewerbung2;
+    static int bewerbungID;
+    static int companyID;
+    static int jobOfferID;
+    static int size;
+    static int studentID;
+
+    @BeforeAll
+    static void beforeAll() {
         try {
-        	listeBewerbung = dao.retrieveAll();
-        	new BewerbungsDAO().create(bewerbung);
-        	listeBewerbung2 = dao.retrieveAll();
-		} catch (Exception e) {
-			fail();
-		}
+            dao = new BewerbungsDAO();
+            bewerbungID = dao.retrieveAll().get(0).getBewerbungsID();
+            companyID = dao.retrieveAll().get(0).getCompanyID();
+            jobOfferID = dao.retrieveAll().get(0).getJobofferID();
+            studentID = dao.retrieveAll().get(0).getStudentID();
+        } catch (Exception e) {
+            fail();
+        }
+
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        try {
+            size = dao.retrieveAll().size();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void testRetrieve() {
+        try {
+            bewerbung = dao.retrieve(bewerbungID);
+        } catch (Exception e) {
+            fail();
+        }
+        assertEquals(bewerbung.getBewerbungsID(), bewerbungID);
+        assertEquals(bewerbung.getJobofferID(), jobOfferID);
+        assertEquals(bewerbung.getCompanyID(), companyID);
+
+        assertNotNull(bewerbung.getBewerbungsdatum());
+        assertNotNull(bewerbung.getNotes());
+    }
+
+    @Test
+    void testRetrieveCompanyBewerbungJobOffer() {
+        try {
+            listeBewerbung = dao.retrieveCompanyBewerbungJobOffer(companyID, jobOfferID);
+            size = listeBewerbung.size();
+        } catch (DatabaseException | SQLException e) {
+            fail();
+        }
+        assertNotNull(listeBewerbung);
+        assertFalse(listeBewerbung.isEmpty());
+
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getJobofferID(), jobOfferID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getJobofferID(), jobOfferID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getJobofferID(), jobOfferID);
+
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+    }
+
+    @Test
+    void testRetrieveCompanyBewerbung() {
+        try {
+            listeBewerbung = dao.retrieveCompanyBewerbung(companyID);
+            size = listeBewerbung.size();
+        } catch (DatabaseException | SQLException e) {
+            fail();
+        }
+        assertNotNull(listeBewerbung);
+        assertFalse(listeBewerbung.isEmpty());
+
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+        assertEquals(listeBewerbung.get((int) (Math.random() * size)).getCompanyID(), companyID);
+
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getBewerbungsdatum());
+
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+        assertNotNull(listeBewerbung.get((int) (Math.random() * size)).getNotes());
+    }
+
+    @Test
+    void testRetrieveAll() {
+        try {
+            listeBewerbung = dao.retrieveAll();
+            size = listeBewerbung.size();
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertTrue(listeBewerbung.size() > 0);
+
+    }
+
+    @Test
+    void testCreate() {
+        bewerbung = new Bewerbung();
+        bewerbung.setCompanyID(companyID);
+        bewerbung.setJobofferID(jobOfferID);
+        bewerbung.setBewerbungsID(0);
+        bewerbung.setNotes("");
+        bewerbung.setStudentID(studentID);
+        bewerbung.setBewerbungsdatum(LocalDate.now());
+        try {
+            listeBewerbung = dao.retrieveAll();
+            new BewerbungsDAO().create(bewerbung);
+            listeBewerbung2 = dao.retrieveAll();
+        } catch (Exception e) {
+            fail();
+        }
         assertNotEquals(listeBewerbung.size(), listeBewerbung2.size());
-    	assertEquals(listeBewerbung.size(), listeBewerbung2.size()-1);
-	}
-	
-	@Test
-	void Testdelete() {
-		//TODO jeder 2. fall fehlerhaft
-		bewerbung = new Bewerbung();
-		try {
-			bewerbung.setBewerbungsID(dao.retrieveAll().get(size-1).getBewerbungsID());
-			listeBewerbung = dao.retrieveAll();
-			dao.delete(bewerbung);
-			listeBewerbung2 = dao.retrieveAll();
-		} catch (DatabaseException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		assertNotEquals(listeBewerbung.size(), listeBewerbung2.size());
-    	assertEquals(listeBewerbung.size(), listeBewerbung2.size()+1);
-			
-	}
-	
-	@Test
-	void Testdelete2() {
-		try {
-			bewerbungID = dao.retrieveAll().get(size-1).getBewerbungsID();
-			System.out.println(bewerbungID);
-			listeBewerbung = dao.retrieveAll();
-			dao.delete(bewerbungID);
-			listeBewerbung2 = dao.retrieveAll();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		assertNotEquals(listeBewerbung.size(), listeBewerbung2.size());
-    	assertEquals(listeBewerbung.size(), listeBewerbung2.size()+1);
-	}
+        assertEquals(listeBewerbung.size(), listeBewerbung2.size() - 1);
+    }
+
+    @Test
+    void testDelete() {
+
+        bewerbung = new Bewerbung();
+        try {
+            bewerbung.setBewerbungsID(dao.retrieveAll().get(size - 1).getBewerbungsID());
+            listeBewerbung = dao.retrieveAll();
+            dao.delete(bewerbung);
+            listeBewerbung2 = dao.retrieveAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotEquals(listeBewerbung.size(), listeBewerbung2.size());
+        assertEquals(listeBewerbung.size(), listeBewerbung2.size() + 1);
+
+    }
+
+    @Test
+    void testDelete2() {
+        try {
+            bewerbungID = dao.retrieveAll().get(size - 1).getBewerbungsID();
+            System.out.println(bewerbungID);
+            listeBewerbung = dao.retrieveAll();
+            dao.delete(bewerbungID);
+            listeBewerbung2 = dao.retrieveAll();
+        } catch (Exception e) {
+            fail();
+            e.printStackTrace();
+        }
+        assertNotEquals(listeBewerbung.size(), listeBewerbung2.size());
+        assertEquals(listeBewerbung.size(), listeBewerbung2.size() + 1);
+    }
 
 }
